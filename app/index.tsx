1917e0fe1bbd5c2ce85f99ea440d7328dfe45264
danyialKhan <danyialkhan667@gmail.com>
2024-12-29 23:57:39 +0500

    layouts

--- app/index.tsx
+++ app/index.tsx
@@ -5,9 +5,9 @@ import { Link } from "expo-router";
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl">Aora!</Text>
-      <Link href="/profile" style={{ color: "blue" }}>
-        Go to profile
+      <Text className="text-3xl font-pblack">Aora!</Text>
+      <Link href="/home" style={{ color: "blue" }}>
+        Go to Home
       </Link>
     </View>
   );
